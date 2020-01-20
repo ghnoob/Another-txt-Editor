5de1457f5050bccc49a7e7383d343de66991259f
ghnoob <ptnchk.rodrigo@gmail.com>
2020-01-20 18:51:40 -0300

    Now cut, copy and delete buttons in the edit menu are disabled if there is not text selected
    TODO: Make similar changes with undo and redo buttons

--- editmenu.py
+++ editmenu.py
@@ -19,37 +19,55 @@ class EditMenu:
         self.parent = parent
         self.menubar = menubar
 
-        editmenu = tk.Menu(self.menubar, tearoff=0)
+        self.create_menu_buttons()
+        self.grey_out()
+        self.parent.textbox.bind('<<Selection>>', lambda event:self.grey_out())
+
+    def create_menu_buttons(self):
+        """Creates the menu buttons for the edit menu."""
+        self.editmenu = tk.Menu(self.menubar, tearoff=0)
         
-        editmenu.add_command(
+        self.editmenu.add_command(
             label='Undo', accelerator='Ctrl+Z',
             command=lambda: self.parent.textbox.event_generate('<<Undo>>')
         )
-        editmenu.add_command(
+        self.editmenu.add_command(
             label='Redo', accelerator='Ctrl+Y',
         command=lambda: self.parent.textbox.event_generate('<<Redo>>')
         )
-        editmenu.add_separator()
-        editmenu.add_command(
+        self.editmenu.add_separator()
+        self.editmenu.add_command(
             label='Cut', accelerator='Ctrl+X',
             command=lambda:self.parent.textbox.event_generate('<<Cut>>')
         )
-        editmenu.add_command(
+        self.editmenu.add_command(
             label='Copy', accelerator='Ctrl+C',
             command=lambda:self.parent.textbox.event_generate('<<Copy>>')
         )
-        editmenu.add_command(
+        self.editmenu.add_command(
             label='Paste', accelerator='Ctrl+V',
             command=lambda:self.parent.textbox.event_generate('<<Paste>>')
         )
-        editmenu.add_separator()
-        editmenu.add_command(
+        self.editmenu.add_separator()
+        self.editmenu.add_command(
             label='Select all', accelerator='Ctrl+A',
             command=lambda:self.parent.textbox.event_generate('<<SelectAll>>')
         )
-        editmenu.add_command(
+        self.editmenu.add_command(
             label='Delete', accelerator='Del',
             command=lambda:self.parent.textbox.event_generate('<<Clear>>')
         )
         
-        self.menubar.add_cascade(label='Edit', menu=editmenu)
+        self.menubar.add_cascade(label='Edit', menu=self.editmenu)
+
+    def grey_out(self):
+        """Disables cut, copy, and delete buttons if there is not text selected."""
+        entries = [3, 4, 8]
+        
+        if self.parent.textbox.tag_ranges('sel'):
+            state = 'normal'
+        else:
+            state = 'disabled'
+        
+        for entry in entries: 
+            self.editmenu.entryconfig(entry, state=state)
