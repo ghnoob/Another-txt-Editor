98334caf5e69f2f76a157baf6d3afcbc5b5a24b6
ghnoob <ptnchk.rodrigo@gmail.com>
2020-02-04 00:02:20 -0300

    Added copyright notices to the source files

--- editmenu.py
+++ editmenu.py
@@ -1,4 +1,20 @@
-"""Interface and commands regarding to edit the text."""
+"""Interface and commands regarding to edit the text.
+
+This file is part of Another txt Editor.
+
+Another txt Editor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Another txt Editor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Another txt Editor.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import tkinter as tk
 
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-"""Main file of the app."""
+"""Another txt Editor Copyright 2020 Rodrigo Pietnechuk.
+
+This file is part of Another txt Editor.
+
+Another txt Editor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Another txt Editor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Another txt Editor.  If not, see <https://www.gnu.org/licenses/>."""
 
 import tkinter as tk
 import tkinter.ttk as ttk
